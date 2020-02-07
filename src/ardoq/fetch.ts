ef8ed52615e70185dc4820b54bf72a35b766f894
Tobias Laundal <tobias.laundal@ardoq.com>
2020-02-07 18:21:50 +0100

    Add rate limiting

--- src/ardoq/fetch.ts
+++ src/ardoq/fetch.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import { RateLimit } from 'async-sema';
 
 const encodeQuery = (query: { [key: string]: string }) =>
   Object.entries(query).reduce(
@@ -7,6 +8,15 @@ const encodeQuery = (query: { [key: string]: string }) =>
     ''
   );
 
+/**
+ * Rate limit for api requests.
+ *
+ * Ardoq's API is generally limited to 50 requests per second, but the component
+ * and reference APIs are limited to 30 requests per second. We limit ourselves
+ * to 20 requests per second to be properly on the safe side.
+ */
+const rateLimit = RateLimit(20);
+
 export const fetchArdoq = async <T = unknown>(
   url: string,
   token: string,
@@ -14,6 +24,7 @@ export const fetchArdoq = async <T = unknown>(
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
   body?: any
 ): Promise<T> => {
+  await rateLimit();
   const response = await fetch(url + encodeQuery(queryParams), {
     method,
     body: body ? JSON.stringify(body) : undefined,
