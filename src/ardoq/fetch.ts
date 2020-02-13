bf1576a7fc37f1337f4fc784f30980e842c05900
Tobias Laundal <tobias.laundal@ardoq.com>
2020-02-13 14:33:36 +0100

    Custom error object for request errors, and user agent

--- src/ardoq/fetch.ts
+++ src/ardoq/fetch.ts
@@ -1,5 +1,7 @@
-import fetch from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
 import { RateLimit } from 'async-sema';
+
+type HttpVerb = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
 const encodeQuery = (query: { [key: string]: string }) =>
   Object.entries(query).reduce(
@@ -7,6 +9,48 @@ const encodeQuery = (query: { [key: string]: string }) =>
       `${acc}${acc.length ? '&' : '?'}${encodeURI(key)}=${encodeURI(value)}`,
     ''
   );
+
+const readResponseBody = async (response: Response) =>
+  response
+    .text()
+    .then(text => {
+      try {
+        return JSON.parse(text);
+      } catch {
+        return text;
+      }
+    })
+    .catch(err => (err.message ? err.message : 'No message'));
+
+export class RequestError extends Error {
+  url: string;
+  method: HttpVerb;
+  headers: { [k: string]: string };
+  requestBody: any;
+  status: number;
+  statusText: string;
+  responseBody: any;
+
+  constructor(
+    url: string,
+    method: HttpVerb,
+    headers: { [k: string]: string },
+    requestBody: any,
+    response: Response,
+    responseBody: any
+  ) {
+    super(
+      `${response.status} ${response.statusText} ${method} ${response.url}`
+    );
+    this.url = url;
+    this.method = method;
+    this.headers = headers;
+    this.requestBody = requestBody;
+    this.status = response.status;
+    this.statusText = response.statusText;
+    this.responseBody = responseBody;
+  }
+}
 
 /**
  * Rate limit for api requests.
@@ -21,18 +65,20 @@ export const fetchArdoq = async <T = unknown>(
   url: string,
   token: string,
   queryParams: { [key: string]: string },
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
+  method: HttpVerb = 'GET',
   body?: any
 ): Promise<T> => {
   await rateLimit();
+  const headers = {
+    Accepts: 'application/json',
+    'User-Agent': 'ardoq-sdk-js/0.1.0',
+    'Content-Type': 'application/json',
+    Authorization: `Token token=${token}`,
+  };
   const response = await fetch(url + encodeQuery(queryParams), {
     method,
     body: body ? JSON.stringify(body) : undefined,
-    headers: {
-      Accepts: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Token token=${token}`,
-    },
+    headers,
   });
 
   if (response.status === 204) {
@@ -40,5 +86,13 @@ export const fetchArdoq = async <T = unknown>(
   } else if (response.ok) {
     return (await response.json()) as T;
   }
-  throw response;
+
+  throw new RequestError(
+    url,
+    method,
+    headers,
+    body,
+    response,
+    await readResponseBody(response)
+  );
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './ardoq/api';
 export * from './ardoq/enums';
+export { RequestError } from './ardoq/fetch';
 export { ApiProperties } from './ardoq/types';
 export { sync } from './sync';
 export * from './sync/simpleGraph';
